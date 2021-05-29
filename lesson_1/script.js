187636d1645f18ac9b845f7067bb406378cee448
Olka White <5543514@gmail.com>
2021-05-29 18:34:39 +0300

    First lesson - creat name document

---
+++ lesson_1/script.js
@@ -0,0 +1,12 @@
+let money = 1000;
+let income = 500;
+let addExpenses = 30;
+let deposit = 50;
+let mission = 3;
+let period = 12;
+
+alert('Скоро сделаем калькулятор');
+
+console.log('У тебя все получится!');
+
+
