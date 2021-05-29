5fe98d38f2af1a6ada6abf60fc50c4c88c5361a3
Olka White <5543514@gmail.com>
2021-05-29 23:06:55 +0300

    first commit on second lesson

---
+++ lesson_2/script.js
@@ -0,0 +1,29 @@
+let money = 1200;
+let income = 500;
+let addExpenses = "Car, Phone, Credit, Utility";
+let deposit = true;
+let mission = 5000;
+let period = 12;
+
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof addExpenses);
+
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' бел рублей.');
+
+let arrExpenses = addExpenses.toLowerCase(); // по логике вещей как-то наверное можно объединить в один метод
+console.log(arrExpenses.split(', '));
+
+let budgetDay = 40;
+console.log(budgetDay);
+
+
+
+
+//alert('Скоро сделаем калькулятор');
+
+//console.log('У тебя все получится!');
+
+
